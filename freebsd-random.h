f9ab18ad1a3dd74e210af55daf53193b82251c86
Andris Raugulis <araugulis@evolutiongaming.com>
2015-04-13 15:39:31 +0300

    Add missing headers.

--- freebsd-random.h
+++ freebsd-random.h
@@ -2,3 +2,5 @@ void freebsd_srandom (unsigned long x);
 long freebsd_random (void);
 void freebsd_srand (unsigned int seed);
 int freebsd_rand (void);
+char * freebsd_initstate(unsigned long seed, char *arg_state, long n);
+char * freebsd_setstate(char *arg_state);
