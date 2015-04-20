986cb68d1aa79b6cc1939e775907f5dfabb09d08
Andris Raugulis <araugulis@evolutiongaming.com>
2015-04-20 14:08:30 +0300

    Make Zend random portable.

--- zend-random.h
+++ zend-random.h
@@ -1,2 +1,14 @@
-int zend_random_ex(unsigned int seed, unsigned long rmax, unsigned int nth);
-int zendom_rand(unsigned int seed, unsigned long rmax);
+#define zend_random_ex32(seed, rnd_max, nth) \
+	({ \
+		__int32_t lseed = seed; \
+		int c=-1; while (c++ < nth) lseed = ((lseed * 1103515245 + 12345) % ((__uint32_t)rnd_max + 1)); \
+		lseed; \
+	})
+#define zend_random_ex64(seed, rnd_max, nth) \
+	({ \
+		__int64_t lseed = seed; \
+		int c=-1; while (c++ < nth) lseed = ((lseed * 1103515245 + 12345) % ((__uint64_t)rnd_max + 1)); \
+		lseed; \
+	})
+#define zend_random32(seed, rnd_max) zend_random_ex32(seed, rnd_max, 0)
+#define zend_random64(seed, rnd_max) zend_random_ex64(seed, rnd_max, 0)
