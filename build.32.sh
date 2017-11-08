0176cc57aeb5da6bcfada8a906c6ccf709776038
Andris Raugulis <moo@arthepsy.eu>
2017-11-08 17:21:37 +0200

    Show test output on failure.

--- build.32.sh
+++ build.32.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd -- `dirname "$0"`
 rm -rf build.32 && mkdir build.32 && cd build.32
-cmake -DCMAKE_C_FLAGS=-m32 .. && make && make test
+cmake -DCMAKE_C_FLAGS=-m32 .. && make && env CTEST_OUTPUT_ON_FAILURE=1 make test
--- build.64.sh
+++ build.64.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd -- `dirname "$0"`
 rm -rf build.64 && mkdir build.64 && cd build.64
-cmake -DCMAKE_C_FLAGS=-m64 cmake .. && make && make test
+cmake -DCMAKE_C_FLAGS=-m64 cmake .. && make && env CTEST_OUTPUT_ON_FAILURE=1 make test
